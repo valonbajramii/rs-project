e4fae6775cb8df3f2cb104dc384efcd95965476d
valobbajramii <valon_bajrami06@outlook.com>
2024-06-24 15:38:46 +0200

    modify

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import Address from "./Pages/Address/Address";
 import Profile from "./Pages/Profile/Profile";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/address" element={<Address />} />
